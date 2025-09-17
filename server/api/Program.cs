ecd41248cb2d82360dddc5b594ece282fbc87762
Kimphung3105 <kimngu01@easv365.dk>
2025-09-17 09:24:05 +0200

    gitignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,32 @@
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql(Environment.GetEnvironmentVariable("MyConnectionString"));
+        });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+{
+    var myTodo = new Todo()
+    {
+        Description = "test",
+        Title = "test title",
+        Id = Guid.NewGuid().ToString(),
+        Isdone = false,
+        Priority = 5
+    };
+
+    dbContext.Todos.Add(myTodo);
+    dbContext.SaveChanges();
+    var objects = dbContext.Todos.ToList();
+    return objects;
+});
 
 app.Run();
