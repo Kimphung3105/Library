3d2993170ef7ded651d54529cdcfa3ec6304c05b
Kimphung3105 <kimngu01@easv365.dk>
2025-09-22 07:41:12 +0200

    deployment

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,32 +1,42 @@
+using System.Text.Json;
+using api;
 using efscaffold.Entities;
 using Infrastructure.Postgres.Scaffolding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+Console.WriteLine(JsonSerializer.Serialize(appOptions));
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    conf.UseNpgsql(Environment.GetEnvironmentVariable("MyConnectionString"));
-        });
+    conf.UseNpgsql(appOptions.DbConnectionString);
 
-var app = builder.Build();
+    var app = builder.Build();
 
-app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
-{
-    var myTodo = new Todo()
+    app.MapGet("/", (
+        [FromServices] IOptionsMonitor<AppOptions> appOptions,
+        [FromServices] MyDbContext dbContext) =>
     {
-        Description = "test",
-        Title = "test title",
-        Id = Guid.NewGuid().ToString(),
-        Isdone = false,
-        Priority = 5
-    };
+        var myTodo = new Todo()
+        {
+            Description = "test",
+            Title = "test title",
+            Id = Guid.NewGuid().ToString(),
+            Isdone = false,
+            Priority = 5
+        };
 
-    dbContext.Todos.Add(myTodo);
-    dbContext.SaveChanges();
-    var objects = dbContext.Todos.ToList();
-    return objects;
+        dbContext.Todos.Add(myTodo);
+        dbContext.SaveChanges();
+        var objects = dbContext.Todos.ToList();
+        return objects;
+    });
+
+    app.Run();
 });
 
-app.Run();
